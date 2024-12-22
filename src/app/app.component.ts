232b4953aadce1605ea34bd55502b13da8f8cbde
Ahmed Hakan Okumus <ahmed.aho666@gmail.com>
2024-12-23 01:38:53 +0300

    #3 - Temel Component Yapılanması

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -3,7 +3,10 @@ import { Component } from '@angular/core';
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
-  styleUrls: ['./app.component.css']
+  // template: ` <p>Burası Template</p>
+  //   <hr />`,
+  styleUrls: ['./app.component.css'],
+  // styles: [`p { color: blue; }`]
 })
 export class AppComponent {
   title = 'gencay-yildiz-angular-egitim';
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,10 +3,16 @@ import { BrowserModule } from '@angular/platform-browser';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { HomeComponent } from './home/home.component';
+import { Com1Component } from './com1/com1.component';
+import { Component1Component } from './component1/component1.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    HomeComponent,
+    Com1Component,
+    Component1Component
   ],
   imports: [
     BrowserModule,
